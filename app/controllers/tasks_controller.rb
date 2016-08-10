9cdc99cc5ab9c6d47370081cebf00c62ad80e285
reignmaker <reignmaker@xakep.ru>
2016-08-10 16:14:03 +0000

    task show template

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,18 @@
 class TasksController < ApplicationController
   before_action :require_login, only: [:new, :edit, :update, :destroy]
+  before_action :load_task, only: [:show, :edit, :update, :destroy]
   def index
     @tasks = Task.includes(:user)
   end
+
+  def show
+
+  end
+
+  protected
+
+  def load_task
+    @task = Task.includes(:user).find(params[:id])
+  end
+
 end
