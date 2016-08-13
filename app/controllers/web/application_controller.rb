ffc6290eb484cbbed31033db2432b69805615cc3
reignmaker <reignmaker@xakep.ru>
2016-08-13 18:48:34 +0000

    render '404' on 'ActiveRecord::RecordNotFound' for 'web' scope

--- app/controllers/web/application_controller.rb
+++ app/controllers/web/application_controller.rb
@@ -1,5 +1,14 @@
 module Web
   class ApplicationController < ::ApplicationController
+    include ::Clearance::Controller
+    # Prevent CSRF attacks by raising an exception.
+    # For APIs, you may want to use :null_session instead.
+    protect_from_forgery with: :exception
+
+    rescue_from ActiveRecord::RecordNotFound do |e|
+      render file: "#{Rails.root}/public/404.html", layout: false, status: 404
+    end
+
   end
 end
 
