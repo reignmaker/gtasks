58d9d58e1b23438a9e968afb8ad53737725e4422
reignmaker <reignmaker@xakep.ru>
2016-08-18 18:46:30 +0000

    remove linebreaks

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,12 @@
 Rails.application.routes.draw do
-
   scope module: :web do
-
     root to: 'welcome#index'
 
     namespace :users, path: :user do
-
       root to: 'welcome#index'
-
       resources :tasks, except: [:index] do
         patch ':state' => 'tasks#change_state', on: :member, as: :change_state
       end
-
     end
-
   end
-
 end
